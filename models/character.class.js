d8f421e981d079fd22a24441ad0b2d92990af3f2
Ysidro <39842830+Ysidr@users.noreply.github.com>
2025-04-22 13:58:56 +0200

    Jumping and Cleaning up

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,5 @@
 class Character extends MovableObject {
-    speed = 4;
+    speed = 7;
     width = 200;
     height = 300;
     framesWalking = [
@@ -10,15 +10,28 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png',
     ];
+    framesJumping = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png',
+    ];
 
     world;
     constructor() {
         super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
         this.y = 480 - this.height - 40;
         this.loadImages(this.framesWalking);
+        this.loadImages(this.framesJumping);
         this.animate();
+        this.applyGravity();
 
-    }
+    };
 
 
 
@@ -26,30 +39,45 @@ class Character extends MovableObject {
 
         setInterval(() => {
             if (this.world.inputs.KeyD && (this.world.isEndlessLevel || this.x < this.world.level.levelEndX)) {
-                this.x += this.speed;
                 this.otherDirection = false;
-                if (this.world.isEndlessLevel == true) {
-                    this.checkForPosition(this.world.backgrounds[4].x + 200 < this.world.character.x || this.world.backgrounds[4].x + 600 < this.world.character.x)
-                }
-
+                this.moveRight();
+                this.isEndlessLevel("right");
             }
             if (this.world.inputs.KeyA && (this.world.isEndlessLevel || this.x > 0)) {
-                this.x -= this.speed;
                 this.otherDirection = true;
-                if (this.world.isEndlessLevel == true) {
-                    this.checkForPosition(this.world.backgrounds[4].x > this.world.character.x || this.world.backgrounds[4].x > this.world.character.x)
-                }
+                this.moveLeft();
+                this.isEndlessLevel("left");
+            }
+
+            if (this.world.inputs.Space && !this.isAboveGround()) {
+                this.jump();
             }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
         setInterval(() => {
+            if (this.isAboveGround()) {
+                this.playAnimation(this.framesJumping);
+            } else {
 
-            if (this.world.inputs.KeyD || this.world.inputs.KeyA) {
-                this.playAnimation(this.framesWalking);
+                if (this.world.inputs.KeyD || this.world.inputs.KeyA) {
+                    this.playAnimation(this.framesWalking);
+                }
             }
         }, 50);
-    }
+    };
+
+    isEndlessLevel(direction) {
+        if (direction == "right") {
+            if (this.world.isEndlessLevel == true) {
+                this.checkForPosition(this.world.backgrounds[4].x + 200 < this.world.character.x || this.world.backgrounds[4].x + 600 < this.world.character.x)
+            }
+        } else if (direction == "left") {
+            if (this.world.isEndlessLevel == true) {
+                this.checkForPosition(this.world.backgrounds[4].x > this.world.character.x || this.world.backgrounds[4].x > this.world.character.x)
+            }
+        }
+    };
 
     checkForPosition(condition) {
         if (condition) {
@@ -63,25 +91,18 @@ class Character extends MovableObject {
                 for (let index = 12; index < 16; index++) {
                     this.world.backgrounds[index].x = this.world.backgrounds[0].x - 899;
                 }
-                this.sortGroup(0, 3)
-                this.sortGroup(0, 3)
-                this.sortGroup(0, 3)
-
-
+                for (let i = 0; i < 3; i++) {
+                    this.sortGroup(0, 3);
+                }
             }
-
-
         }
-
     };
 
     sortGroup(startIndex, endIndex) {
         const group = this.world.backgrounds.slice(startIndex, endIndex + 1);
         this.world.backgrounds.splice(startIndex, endIndex + 1)
         this.world.backgrounds.push(...group);
-    }
+    };
 
-    jump() {
 
-    }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -2,6 +2,8 @@ class MovableObject {
     x = 50;
     y = 250
     img;
+    speedY = 0;
+    acceleration = 2.5;
     renderFrames = {}
     currentImage = 0;
     speed = 0.15;
@@ -21,17 +23,35 @@ class MovableObject {
         });
     }
 
-    moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-            
-        }, 1000 / 60);
+    playAnimation(images) {
+        let i = this.currentImage % images.length;
+        let path = images[i];
+        this.img = this.renderFrames[path];
+        this.currentImage++;
     }
 
-    playAnimation(images){
-        let i = this.currentImage % images.length;
-            let path = images[i];
-            this.img = this.renderFrames[path];
-            this.currentImage++;
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround() {
+        return this.y < 480 - this.height - 40;
+    }
+
+    jump() {
+        this.speedY = 30;
+    }
+
+    moveRight() {
+        this.x += this.speed;
+    }
+
+    moveLeft() {
+        this.x -= this.speed;
     }
 }
